35e03a11b03cef274d73789101596089b73c07fd
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2016-06-26 12:34:44 -0700

    flat migration to Revit 2017

--- CustomExporterAdnMeshJson/Properties/AssemblyInfo.cs
+++ CustomExporterAdnMeshJson/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "Revit Add-In Title" )]
-[assembly: AssemblyDescription( "Revit Add-In Description" )]
+[assembly: AssemblyTitle( "CustomExporterAdnMeshJson" )]
+[assembly: AssemblyDescription( "C# .NET Revit API add-in geometry CustomExporter to ADN mesh JSON format" )]
 [assembly: AssemblyConfiguration( "" )]
 [assembly: AssemblyCompany( "Autodesk Inc." )]
-[assembly: AssemblyProduct( "Revit Add-In Wizard Template Application" )]
-[assembly: AssemblyCopyright( "Copyright 2013 © Jeremy Tammik Autodesk Inc." )]
+[assembly: AssemblyProduct( "CustomExporterAdnMeshJson" )]
+[assembly: AssemblyCopyright( "Copyright 2013-2016 © Jeremy Tammik Autodesk Inc." )]
 [assembly: AssemblyTrademark( "" )]
 [assembly: AssemblyCulture( "" )]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.0.0.0" )]
-[assembly: AssemblyFileVersion( "1.0.0.0" )]
+[assembly: AssemblyVersion( "2017.0.0.0" )]
+[assembly: AssemblyFileVersion( "2017.0.0.0" )]
